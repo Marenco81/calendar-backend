a49c6dee44dfc362c22f0cf1acffece17f8b9ba5
Alvaro Marenco <alvaromarencom@gmail.com>
2025-05-07 17:34:05 -0600

    Creating User Routes

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+/*
+    Rutas de Usuarios / Auth
+    host + /api/auth
+*/
+
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+});
+
+module.exports = router;
