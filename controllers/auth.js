058e30451e1ff28de86eb81bafc5f8b82638ccd0
Alvaro Marenco <alvaromarencom@gmail.com>
2025-05-07 18:57:12 -0600

    Retrieve POST information

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,34 @@ const express = require('express');
 
 const createUser = (req, res = express.response) => {
 
+    const {name, email, password} = req.body;
+
+    if( name.length < 5 ) {
+        return res.status(400).json({
+            ok:false,
+            msg: 'El nombre debe contener al menos 5 letras'
+        })
+    }
+
     res.json({
         ok: true,
-        msg: 'registro'
+        msg: 'registro',
+        name,
+        email,
+        password
+
     })
 };
 
 const loginUser = (req, res = express.response) => {
 
+    const {email, password} = req.body;
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     })
 };
 
