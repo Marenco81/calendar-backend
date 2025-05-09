11212e0d7828e97e9d532204f4aa6c413c613921
Alvaro Marenco <alvaromarencom@gmail.com>
2025-05-09 12:35:58 -0600

    User Login Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,16 +46,49 @@ const createUser = async (req, res = express.response) => {
     
 };
 
-const loginUser = (req, res = express.response) => {
+const loginUser = async (req, res = express.response) => {
 
     const {email, password} = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        const user = await User.findOne({email});
+        console.log(user);
+
+        if(!user) {
+            res.status(400).json({
+                ok: false,
+                msg: 'No existing User with that email'
+            });
+        }
+        //Confirmar password
+
+        const validPassword = bcrypt.compareSync( password, user.password );
+
+        if(!validPassword) {
+            res.status(400).json({
+                ok: false,
+                msg: 'Incorrect Password'
+            });
+        }
+
+        // Generar JWT
+
+        res.json({
+            ok: true,
+            msg: 'login',
+            uid: user.id,
+            name: user.name,
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact your DB admin'
+        });
+    }
+
+    
 };
 
 const renewToken = (req, res = express.response) => {
