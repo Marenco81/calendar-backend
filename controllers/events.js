7fe7a50bdc16dd20548bbff178e3d10adbdb0d1b
Alvaro Marenco <alvaromarencom@gmail.com>
2025-05-16 13:38:49 -0600

    Updating an Event

--- controllers/events.js
+++ controllers/events.js
@@ -41,12 +41,48 @@ const crearEvento = async (req, res = response) => {
 
 };
 
-const actualizarEvento = (req, res = response) => {
+const actualizarEvento = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'actualizarEvento'
-    })
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        const event = await Events.findById(eventId);
+
+        if( !event ) {
+            res.status(404).json({
+                ok: false,
+                msg: "No Event with that id"
+            });
+        }
+
+        if (event.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No privilege to edit this event'
+            });
+        }
+
+        const newEvent = {
+            ...req.body, 
+            user: uid
+        }
+
+        const eventUpdated = await Events.findByIdAndUpdate( eventId, newEvent, {new:true} );
+
+        res.json({
+            ok: true,
+            msg: eventUpdated
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact DB Admin'
+        });
+    }
+
 
 };
 
